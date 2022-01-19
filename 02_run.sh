5d6231a4f6d5b49ea32a9307e8d926eb5076fead
Ian Caldas <ivc2@cornell.edu>
2022-01-19 14:33:13 -0500

    SLiM 3.7 for simulations

--- 02_run.sh
+++ 02_run.sh
@@ -3,5 +3,5 @@ CONFIGFILES=$(ls 02_simulation-parameters/*.yaml)
 
 for file in ${CONFIGFILES}
 do
-    snakemake -c4 --use-conda --snakefile 02_simulate.smk --configfile ${file} "$@" --config slim=bin/slim3.6 normalization_stats=resources/normalization-stats.tsv simulations=3 use_subdirectory=true
+    snakemake -c4 --use-conda --snakefile 02_simulate.smk --configfile ${file} "$@" --config slim=bin/slim3.7 normalization_stats=resources/normalization-stats.tsv simulations=3 use_subdirectory=true
 done
