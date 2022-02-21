a44c005643e8f79df8f7e913844f8414ab35f1be
Ian Caldas <ivc2@cornell.edu>
2022-02-21 14:36:39 -0500

    Submit simulation jobs in better array arrangements for slurm.

--- 02_submit-simulation-jobs.sh
+++ 02_submit-simulation-jobs.sh
@@ -1,4 +1,16 @@
-sbatch --array=1-1000 -J neutral 02_slurm-simulate.sh --configfile ./02_simulation-parameters/neutral.yaml
-sbatch --array=1-10000 -J hard 02_slurm-simulate.sh --configfile ./02_simulation-parameters/hard.yaml
-sbatch --array=1-20000 -J rnm 02_slurm-simulate.sh --configfile ./02_simulation-parameters/rnm.yaml
-sbatch --array=1-20000 -J sgv 02_slurm-simulate.sh --configfile ./02_simulation-parameters/sgv.yaml
+# sbatch --array=1-1000 -J neutral 02_slurm-simulate.sh --configfile ./02_simulation-parameters/neutral.yaml
+
+for i in {1..5}
+do
+    sbatch --array=1-1000 -J hard${i} 02_slurm-simulate.sh --configfile ./02_simulation-parameters/hard.yaml
+done
+
+for i in {1..15}
+do
+    sbatch --array=1-1000 -J rnm${i} 02_slurm-simulate.sh --configfile ./02_simulation-parameters/rnm.yaml
+done
+
+for i in {1..15}
+do
+    sbatch --array=1-1000 -J sgv${i} 02_slurm-simulate.sh --configfile ./02_simulation-parameters/sgv.yaml
+done
