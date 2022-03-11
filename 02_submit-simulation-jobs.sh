a270cabc36f1e59e4f0a620ab840234421b4a7f6
Ian Caldas <ivc2@cornell.edu>
2022-03-11 15:39:20 -0500

    Clean simulation submissions on cluster

--- 02_submit-simulation-jobs.sh
+++ 02_submit-simulation-jobs.sh
@@ -1,16 +1,7 @@
-# sbatch --array=1-1000 -J neutral 02_slurm-simulate.sh --configfile ./02_simulation-parameters/neutral.yaml
-
-for i in {1..5}
-do
-    sbatch --array=1-1000 -J hard${i} 02_slurm-simulate.sh --configfile ./02_simulation-parameters/hard.yaml
-done
-
-for i in {1..15}
-do
-    sbatch --array=1-1000 -J rnm${i} 02_slurm-simulate.sh --configfile ./02_simulation-parameters/rnm.yaml
-done
-
-for i in {1..15}
-do
-    sbatch --array=1-1000 -J sgv${i} 02_slurm-simulate.sh --configfile ./02_simulation-parameters/sgv.yaml
-done
+sbatch --array=1-1000 -J basic-neutral 02_slurm-simulate.sh --configfile ./02_simulation-parameters/neutral.yaml
+sbatch --array=1-1000 -J codominant-hard 02_slurm-simulate.sh --configfile ./02_simulation-parameters/hard.yaml
+sbatch --array=1-1000 -J codominant-rnm 02_slurm-simulate.sh --configfile ./02_simulation-parameters/rnm.yaml
+sbatch --array=1-1000 -J codominant-sgv 02_slurm-simulate.sh --configfile ./02_simulation-parameters/sgv.yaml
+sbatch --array=1-1000 -J dominant-hard 02_slurm-simulate.sh --configfile ./02_simulation-parameters/dominant-hard.yaml
+sbatch --array=1-1000 -J dominant-rnm 02_slurm-simulate.sh --configfile ./02_simulation-parameters/dominant-rnm.yaml
+sbatch --array=1-1000 -J dominant-sgv 02_slurm-simulate.sh --configfile ./02_simulation-parameters/dominant-sgv.yaml
